be3412d9912699c8baf1fa8859ad71f2d62f75a1
whiteFlash <ivankotov375@gmail.com>
2020-07-14 11:03:46 +1100

    CRUD finnished. Comments added to methods and variables.

--- Yumasoft/src/app/appform/appform.component.ts
+++ Yumasoft/src/app/appform/appform.component.ts
@@ -11,39 +11,50 @@ import { Model } from './model';
 })
 export class AppformComponent implements OnInit {
 
+  //Injection of HttpClient to component
   constructor(private http: HttpClient) { }
+  
+  //Array is avaliable for manipulations on the 
+  //load stage of the app.
   ngOnInit(): void {
     this.http.get('assets/data.json')
               .subscribe((data:Model[]) => this.JSONString = data);
     
   }
 
+  //Array to load data from JSONFile.
   ArrayOfJson: Model[];
+  //Array to work with data in the working process of app.
   JSONString:  any;
-  name: string = "";
-  year: any = 0;
 
+  //variables store date for the Model fields class.
+  name: string = "";
+  year: any = "";
+
+  //Unload data into textarea from JSON file.
+  //JSON array is stored by this path => assets/data.json
   UnloadJson():void {
     this.http.get('assets/data.json')
              .subscribe((data:Model[]) => this.ArrayOfJson = data);             
   }
 
-  Show(textBox: any) : void{
+  //Lodaing data from textarea to array that will be manupulated.
+  LoadJsonData(textBox: any) : void{
     this.JSONString = JSON.parse(textBox);
     console.log(this.JSONString);
   }
 
-  test($event:any):void 
+  //Method selects element from table. 
+  //And populate inputfields with data to correct info.
+  SelectElement($event:any):void 
   {
     if($event.target.value as string)
     {
       this.name = $event.target.value;
-      console.log($event.target.value);
     }
     else
     {
       this.year = $event.target.value;
-      console.log($event.target.value);
     }
 
     for(var i = 0; i <= this.JSONString.length - 1; i++)
@@ -53,26 +64,44 @@ export class AppformComponent implements OnInit {
         this.year = this.JSONString[i].year;
       }
     }
-    console.log(this.name + " " + this.year);
-
   }
 
-  Update(name, year):void{
-    var x = new Model();
-    x.name = name;
-    x.year = year;
+  //Update JSONString array with selected element.
+  Update(name, year):void
+  {
+    var model = new Model();
+    model.name = name;
+    model.year = year;
     console.log(this.JSONString);
     for(var i = 0; i <= this.JSONString.length - 1; i++)
     {
-        if(x.name == this.JSONString[i].name || this.JSONString[i].year == x.year)
+        if(model.name == this.JSONString[i].name || this.JSONString[i].year == model.year)
         {
-            this,this.JSONString[i] = x;
+          this,this.JSONString[i] = model;
         }
     }
-    console.log(this.JSONString);
-    console.log(x.name + " " + x.year);
+  }
 
+  //Create new element in JSONString array.
+  Create(name, year):void
+  {
+    var model = new Model();
+    model.name = name;
+    model.year = year;
+    this.JSONString.push(model);
+  }
 
+  //Delete elemente from JSONString array.
+  Delete(name, year){
+    if(name != null || year != null){
+      for(var i = 0; i <= this.JSONString.length - 1; i++)
+      {
+        if(name == this.JSONString[i].name || this.JSONString[i].year == year)
+        {
+          this.JSONString.splice(i,1);
+        }
+      }
+    }
   }
 
 }
